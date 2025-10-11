efd5a744ec362458e9a4e1c70794372e5d0c1650
towacott <snakt@outlook.jp>
2025-10-11 20:04:05 +0900

    favicon remove

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Feel Salad';
-export const SITE_DESCRIPTION = 'test';
+export const SITE_DESCRIPTION = '';
